9b9eaf41ad0cd07544256e0ae97dd653edf37a2c
kenta0429 <siany.hiyoko@gmail.com>
2021-08-16 18:43:26 +0900

    portfolio1

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,11 @@ Route::get('/', function () {
 Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
