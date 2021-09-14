792a103bf5fbf424e52437d4653d8d4841679231
kenta0429 <siany.hiyoko@gmail.com>
2021-09-14 23:06:50 +0900

    db

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,8 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->text('profile',200);
+            $table->string('ruby_name')->nullable();
+            $table->text('profile',200)->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
