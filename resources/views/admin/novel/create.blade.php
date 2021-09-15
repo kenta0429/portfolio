bae6033d6af468da3084e55721afc4f8ddddef24
kenta0429 <siany.hiyoko@gmail.com>
2021-09-15 16:43:37 +0900

    controller

--- resources/views/admin/novel/create.blade.php
+++ resources/views/admin/novel/create.blade.php
@@ -1,15 +1,13 @@
 @extends('layouts.admin')
 
 
-{{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
-@section('title', 'ニュースの新規作成')
+@section('title', '新規小説作成')
 
-{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
 @section('content')
 <div class="container">
     <div class="row">
         <div class="col-md-8 mx-auto">
-            <h2>ニュース新規作成</h2>
+            <h2>新規小説作成</h2>
             <form action="{{ action('Admin\NovelController@create') }}" method="post" enctype="multipart/form-data">
 
                 @if (count($errors) > 0)
