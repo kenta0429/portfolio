878db3e3f8f390710b245a444c52ba9a0c11069a
kenta0429 <siany.hiyoko@gmail.com>
2021-09-15 16:51:31 +0900

    app php

--- app/Novel.php
+++ app/Novel.php
@@ -6,17 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Novel extends Model
 {
-    protected $guarded = array('id');
+  protected $guarded = array('id');
 
-    // 以下を追記
-    public static $rules = array(
-        'title' => 'required',
-        'body' => 'required',
-    );
-    
-     public function histories()
-    {
-      return $this->hasMany('App\History');
+  // 以下を追記
+  public static $rules = array(
+    'title' => 'required',
+    'body' => 'required',
+  );
 
-    }
+  public function histories()
+  {
+    return $this->hasMany('App\History');
+  }
 }
